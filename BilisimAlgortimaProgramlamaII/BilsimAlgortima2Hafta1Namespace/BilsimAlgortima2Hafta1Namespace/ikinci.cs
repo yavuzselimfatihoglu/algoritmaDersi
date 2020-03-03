9e75bb01bc924ae55060f8bfa01dc73cbfcce2ed
yselim <yavuz.selim.fatihoglu@gmail.com>
2020-03-04 02:00:25 +0300

    Add files via upload

---
+++ BilisimAlgortimaProgramlamaII/BilsimAlgortima2Hafta1Namespace/BilsimAlgortima2Hafta1Namespace/ikinci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ikinci
+{
+    class Program
+    {
+      public static int topla(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
