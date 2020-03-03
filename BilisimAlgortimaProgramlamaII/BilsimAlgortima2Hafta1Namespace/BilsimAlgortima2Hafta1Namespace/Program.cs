a97ab5e465611d4c560f3a20b5648bfa1a3a3dda
yselim <yavuz.selim.fatihoglu@gmail.com>
2020-03-04 02:11:10 +0300

    ekle

---
+++ BilisimAlgortimaProgramlamaII/BilsimAlgortima2Hafta1Namespace/BilsimAlgortima2Hafta1Namespace/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ikinci;
+namespace ilk
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+        
+           Console.WriteLine("Sonuc:" +ikinci.Program.topla(5,6));
+            
+        }
+     
+    }
+}
