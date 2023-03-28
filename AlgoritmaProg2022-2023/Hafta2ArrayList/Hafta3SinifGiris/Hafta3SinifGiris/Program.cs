4b6b7a8b41709f4127a5f4b53fd18410397e2e6a
yselim <yavuz.selim.fatihoglu@gmail.com>
2023-03-28 11:50:52 +0300

    Add files via upload

---
+++ AlgoritmaProg2022-2023/Hafta2ArrayList/Hafta3SinifGiris/Hafta3SinifGiris/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Hafta3SinifGiris
+{
+    class Kart
+    {
+        //üye değişkenler
+        string musAdSoyad;
+        double bakiye;
+        double limit;
+        int musNo;
+
+        //üye metotlar
+        //paraCek(miktar)
+        //paraYatir(miktar)
+        //hesapYazdir()
+        //hesapAc()
+
+        //kurucu ve yıkıcı metotlar
+        //get/set anahtarların görevi
+
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
